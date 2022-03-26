f063e639df08ca606f779fc771040a494655849f
Irina Wang <irina.wang@outlook.com>
2022-03-26 14:45:27 -0400

    official file for collecting data points

--- Wand/data_collection.py
+++ Wand/data_collection.py
@@ -25,54 +25,36 @@ if __name__ == "__main__":
     button_pressed = 1
     prev_button_pressed = 1
     print("hello world")
-    arduino = serial.Serial(port='/dev/tty.usbmodem142101', baudrate=115200, timeout=timeout)
+    arduino = serial.Serial(port='/dev/cu.usbmodem1444301', baudrate=115200, timeout=timeout)
     if True:
-        
-        f = open('data.csv', 'w')
-        writer = csv.writer(f)
         k = 0
         while k < 20:
             print('Collecting the ' + str(k) + 'th Data -------------')
-            time.sleep(3)
-            i = 0
-            entry = []
+            time.sleep(1)
+
             entry_np = None
             # collect motion data for 3s
-            while i < 300:
+            while (entry_np is None or len(entry_np) <= 300):
                 data = arduino.readline()
                 if (data is not None and len(data) > 0):
-                    print("here")
+                    # print("here")
                     prev_button_pressed = button_pressed
                     (array, button_pressed) = read_data_from_serial(data)
-                    # print("array = " + str(array[0]))
-                    # print(type(array))
-                    entry.append(array)
-                    # print(entry_np.shape)
-                    # print(array.shape)
                     if entry_np is None:
                         entry_np = np.expand_dims(array,0)
                     else:
                         entry_np = np.append(entry_np,np.expand_dims(array,0),axis=0)
-                        # entry_np = np.append(entry_np,array,axis=1)
-                        # print(entry_np.shape)
-                    
-                i += 1
                 time.sleep(0.01)
             print('im out, adding ---------')
-            # print(entry)
-            writer.writerow(entry)
+           
 
             k += 1
             entry_np = np.reshape(entry_np,(-1,6))
             print(entry_np.shape)
-            np.savetxt('./test/data_'+str(k)+'_np.csv', entry_np, delimiter=',')
-        f.close()
+
+            np.savetxt('./new/data_'+str(k)+'_np.csv', entry_np, delimiter=',')
         
             # print("button pressed = " + str(button_pressed))
-        
-    
-
-
         # if button_released(button_pressed, prev_button_pressed):
         #     train_new_model()
         #     pass 
