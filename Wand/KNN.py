1aadc868f1262b6e342762c4abc860a8d683bd2a
Irina Wang <irina.wang@outlook.com>
2022-03-26 15:53:57 -0400

    cleaned KNN

--- Wand/KNN.py
+++ Wand/KNN.py
@@ -1,21 +1,13 @@
 import numpy as np
 import pandas as pd
-
-# from sklearn.datasets import make_blobs, make_circles
 from sklearn.neighbors import KNeighborsClassifier
-# from sklearn import preprocessing
-
-# import matplotlib.pyplot as plt
-# from mpl_toolkits import mplot3d 
-# %matplotlib inline
-# import seaborn as sns; sns.set()
 
 # --------------------------------------------------------------
 # Importing Training Data
 SAMPLE_SIZE = 20
 
 def import_data(label,n):
-    data = np.zeros((20,300,6))
+    data = np.zeros((n,300,6)) # pretrained dimension
     for i in range(n):
         f = './' + str(label) + '/data_' + str(i+1) + '_np.csv'
         data[i] = np.loadtxt(f, delimiter=',')
@@ -24,28 +16,15 @@ def import_data(label,n):
 wave_Xs = import_data('wave', SAMPLE_SIZE)
 swipe_Xs = import_data('swipe', SAMPLE_SIZE)
 spin_Xs = import_data('spin', SAMPLE_SIZE)
-new_Xs = import_data('new', SAMPLE_SIZE)
+# new_Xs = import_data('new', SAMPLE_SIZE)
 
 wave_y =  np.full(SAMPLE_SIZE, 0)
 swipe_y = np.full(SAMPLE_SIZE, 1)
 spin_y = np.full(SAMPLE_SIZE, 2)
-new_y = np.full(SAMPLE_SIZE, 3)
+# new_y = np.full(SAMPLE_SIZE, 3)
 
 trainY = np.concatenate((wave_y,swipe_y,spin_y))
-
-# --------------------------------------------------------------
-# Importing Testing Data
-def import_category_data(label, sample_size, index):
-    data = []
-    f = './' + str(label) + '/data_' + str(index) + '_np.csv'
-    data.append(pd.read_csv(f))
-    return data
-
-# import test data 
-wave_testX = import_category_data('test', 1, 1)
-swipe_testX  = import_category_data('test', 1, 5)
-spin_testX  = import_category_data('test', 1, 8)
-
+# trainY = np.concatenate((wave_y,swipe_y,spin_y,new_y))
 
 # --------------------------------------------------------------
 ## Feature Engineering 
@@ -55,36 +34,39 @@ def feature_engineering(d):
     d_prime[:, 3] = d_prime[:, 3] * 100 # scale accel x
     return d_prime[:,[1,2,3]] # select gyro y, gyro z, accel x
 
-# n = feature_engineering(new_Xs) 
-
-w = feature_engineering(wave_Xs)
+wa = feature_engineering(wave_Xs)
 sw = feature_engineering(swipe_Xs)
 sp = feature_engineering(spin_Xs) 
+# nw = feature_engineering(new_Xs) 
 
 # Concat Xs, y
-trainX = np.concatenate((w,sw,sp))
+trainX = np.concatenate((wa,sw,sp))
+# trainX = np.concatenate((wa,sw,sp,nw))
 
 # --------------------------------------------------------------
-## Feature Engineering 
-# Take sd of one feature
+# Importing Testing Data
+def import_test_data(label, index):
+    data = []
+    f = './' + str(label) + '/data_' + str(index) + '_np.csv'
+    data.append(pd.read_csv(f))
+    return data
 
-w_ = feature_engineering(wave_testX, 1)
-sw_ = feature_engineering(swipe_testX, 1)
-sp_ = feature_engineering(spin_testX, 1)
+# import test data 
+wave_testX = import_test_data('test', 1, 1)
+swipe_testX  = import_test_data('test', 1, 5)
+spin_testX  = import_test_data('test', 1, 8)
 
-w_3 = w_[:,[1,2,3]]
-sw_3 = sw_[:,[1,2,3]]
-sp_3 = sp_[:,[1,2,3]]
-
-w_3[:, 2] = w_3[:, 2] * 100
-sw_3[:, 2] = sw_3[:, 2] * 100
-sp_3[:, 2] = sp_3[:, 2] * 100
+test1 = feature_engineering(wave_testX)
+test2 = feature_engineering(wave_testX)
+test3 = feature_engineering(wave_testX)
 
 # --------------------------------------------------------------
 ## Predict
-# Take sd of one feature
+def predict_class(model, testX):
+    return model.predict(testX)
+
 if __name__ == '__main__': 
     knn_clf = KNeighborsClassifier(n_neighbors=5)
-    knn_clf.fit(Xs_, y)
-    ypred=knn_clf.predict(w_3)
+    knn_clf.fit(trainX, trainY)
+    ypred = knn_clf.predict(test1)
     print(ypred)
