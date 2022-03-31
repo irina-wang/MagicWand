c860b4cfc335060d36e1b6c5725ab5cad936643e
Irina Wang <irina.wang@outlook.com>
2022-03-31 18:04:33 -0400

    change port

--- Wand/_constants.py
+++ Wand/_constants.py
@@ -2,10 +2,13 @@ from KNN import SAMPLE_SIZE
 
 # PORT! change here!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 PORT = '/dev/cu.usbmodem1434301'
+PORTOUT = '/dev/cu.usbmodem1434101'
+
 
 TRAINING = 'Sound/training_effect.wav'
 FINISHED_TRAINING = 'Sound/finished_training.wav'
 
 dim = 50 # how many data are we taking std of
-BUTTON = [0 for i in range(30)] +  [1 for i in range(10)] + [0 for i in range(500)] + [1 for i in range(100)] 
+BUTTON = [1 for i in range(30000)] 
+# BUTTON = [0 for i in range(30)] +  [1 for i in range(10)] + [0 for i in range(500)] + [1 for i in range(100)] 
 # [0 for i in range(10)] + [1 for i in range(1000)] + 
