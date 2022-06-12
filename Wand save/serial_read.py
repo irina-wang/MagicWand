e900c58c758b49a7b577038e01c8c476c031325a
Irina Wang <irina.wang@outlook.com>
2022-06-12 05:07:47 -0400

    update comments

--- Wand save/serial_read.py
+++ Wand save/serial_read.py
@@ -1,14 +1,13 @@
-from random import randint
+from random import randint, randrange
 from wsgiref.validate import ErrorWrapper
-import serial 
-import numpy as np
-import time
-from playsound import playsound
-import KNN
 from sklearn.neighbors import KNeighborsClassifier
-import _constants as my
+from playsound import playsound
 
-from random import randrange
+import time
+import serial
+import numpy as np
+import KNN
+import _constants as my
 
 
 """
@@ -17,6 +16,9 @@ columns are: [gyro_x, gyro_y, gyro_z, accel_x, accel_y, accel_z, button_pressed]
 
 PORT = my.PORT
 PORT2 = my.PORTOUT
+
+TRAINING = my.TRAINING
+FINISHED_TRAINING = my.FINISHED_TRAINING
 
 # pretrained model
 X = KNN.trainX
@@ -28,15 +30,14 @@ model =  knn_clf.fit(X, Y)
 testD = None # [50,6]
 new_class = None
 
-TRAINING = my.TRAINING
-FINISHED_TRAINING = my.FINISHED_TRAINING
+# new class
+NEWCLASS = 4
 
- # 1 is not pressed, 0 is pressed
+# button values
 PRESSED = 0
 RELEASED = 1
 
-# new class
-NEWCLASS = 4
+
 
 def read_data_from_serial(bytes_string):
     data = bytes_string.decode('UTF-8')
@@ -49,11 +50,8 @@ def gather_data(array, entry_np):
         entry_np = np.expand_dims(array,0)
     else:
         entry_np = np.append(entry_np,np.expand_dims(array,0),axis=0)
-        # time.sleep(0.01)
     return entry_np
 
-
-# check new data 
 # process new data 
 def slice_new_data():
     n = int(len(new_class)/50)
@@ -63,11 +61,11 @@ def slice_new_data():
 
 # Train New Model
 def train_model():
-    # Train KNN model 
     print('Training /\/\/\/\/\/\/\/\/\/\/\/\/')
     playsound(TRAINING) # signal start
     d,n = slice_new_data()
-# process new data
+
+    # process new data
     x, n = KNN.reshape_X(d)
     newX = KNN.feature_engineering(x,1)
     newY = KNN.make_Y(n, NEWCLASS)
@@ -84,7 +82,7 @@ def train_model():
 arduino_samp_freq_Hz = 100
 timeout = 1/arduino_samp_freq_Hz
 
-# Main
+# ---- Main ----
 if __name__ == "__main__":
     button_pressed = RELEASED # 1
     prev_button_status = RELEASED # 1
@@ -93,17 +91,13 @@ if __name__ == "__main__":
     print("hello world")
     arduino = serial.Serial(port=PORT, baudrate=115200, timeout=timeout)
     arduinoOUT = serial.Serial(port=PORT2, baudrate=9600, timeout=timeout)  
-    
-    # arduinoOUT = serial.Serial(port=PORT2, baudrate=9600, timeout=timeout) # write the prediction
+
     while True:
         arduinoOUT.reset_input_buffer()
         serial_data = arduino.readline()
         
         if (serial_data is not None and len(serial_data) > 0):
             (array, button_status) = read_data_from_serial(serial_data)
-        # for testing purpose 
-            # button_status = randrange(2) # generate 0,1
-            # button_status = my.BUTTON[i] # testing code
             print(button_status)
             if button_status == RELEASED and prev_button_status == RELEASED:
                 testD = gather_data(array, testD) 
@@ -124,12 +118,11 @@ if __name__ == "__main__":
                 if (len(new_class) >= 100): # try this out
                     model = train_model()
                     arduinoOUT.reset_input_buffer()
-                    # arduinoOUT.reset_input_buffer() # something like this 
                     new_class = None
                 else:
                     print("Oops!  Too short.  Try press longer...")
-                # Train 
-                prev_button_status = RELEASED
+                 
+                prev_button_status = RELEASED # Training
 
             else: # button_status == PRESSED
                 new_class = gather_data(array, new_class) 
